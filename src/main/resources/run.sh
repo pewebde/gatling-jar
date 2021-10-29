64ae04fca3a531733b488273cf2b430e8e92672a
Peter Erben <pe@peweb.de>
2021-10-29 11:36:46 +0200

    Add new Testcase on localhost:8080

--- src/main/resources/run.sh
+++ src/main/resources/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 USER_ARGS=""
-
 COMPILATION_CLASSPATH=`find -L . -maxdepth 1 -name "*.jar" -type f -exec printf :{} ';'`
+JAVA_OPTS="-DbaseUrl=http://localhost:8080  -DdurationMin=0.25 -DrequestPerSecond=10"
+SIMULATION_NAME="gatling.test.example.simulation.ExampleSimulation"
 
-#java $JAVA_OPTS $USER_ARGS -cp $COMPILATION_CLASSPATH io.gatling.app.Gatling -s MySimulation
-
-java -cp $COMPILATION_CLASSPATH io.gatling.app.Gatling -s MySimulation
+#java ${JAVA_OPTS} -cp ${COMPILATION_CLASSPATH} io.gatling.app.Gatling -s gatling.test.simulation.ExampleSimulation
+java ${JAVA_OPTS} -cp ${COMPILATION_CLASSPATH} io.gatling.app.Gatling -s ${SIMULATION_NAME}
